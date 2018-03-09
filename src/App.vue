b2077b9394104c07000f626e172164902b5e1de2
LaptopGit <36667491+Hazaaa@users.noreply.github.com>
2018-03-09 13:25:45 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!--Templejt deo komponente-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h2>{{ msg }}</h2> <!--Pristupa msg atributu-->
+  </div>
+</template>
+
+<script>
+//objekat koji vraca ova komponenta
+export default {
+  data () {
+    return {
+      msg: 'Vue.js komponenta'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
